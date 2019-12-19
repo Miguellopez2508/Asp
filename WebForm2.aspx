c518074103303e84cb2047919e31a649d2e5d3d9
unknown <Elorrieta Errekamari@INFP4A101-20.H015112.NET>
2019-12-19 14:01:10 +0100

    primer filtro

---
+++ WebForm2.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm2.aspx.vb" Inherits="Inicio_Sesion.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Filtros de Busqueda : "></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Nombre"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="NombreTB" runat="server" Width="240px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label3" runat="server" Text="Tipo de alojamiento: "></asp:Label>
+            <asp:DropDownList ID="alojamientoDDL" runat="server" AutoPostBack="True">
+                <asp:ListItem></asp:ListItem>
+                <asp:ListItem>Alojamiento Rural</asp:ListItem>
+                <asp:ListItem>Albergue</asp:ListItem>
+                <asp:ListItem>Camping</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="BuscarBtn" runat="server" Text="Buscar" />
+            <br />
+            <br />
+            <asp:Label ID="resultadolabel" runat="server"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm2.aspx.designer.vb
@@ -0,0 +1,87 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class WebForm2
+    
+    '''<summary>
+    '''Control form1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+    
+    '''<summary>
+    '''Control Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control Label2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label2 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control NombreTB.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents NombreTB As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control Label3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label3 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control alojamientoDDL.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents alojamientoDDL As Global.System.Web.UI.WebControls.DropDownList
+    
+    '''<summary>
+    '''Control BuscarBtn.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents BuscarBtn As Global.System.Web.UI.WebControls.Button
+    
+    '''<summary>
+    '''Control resultadolabel.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents resultadolabel As Global.System.Web.UI.WebControls.Label
+End Class
